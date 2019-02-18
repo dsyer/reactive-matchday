3c5ae9763ecbc9713f6c647e7ebed64341c48e5a
Dave Syer <dsyer@pivotal.io>
2019-02-18 11:15:50 +0000

    Explicit support for SSE

--- src/main/java/com/github/danielfernandez/matchday/web/view/ReactiveMustacheView.java
+++ src/main/java/com/github/danielfernandez/matchday/web/view/ReactiveMustacheView.java
@@ -16,6 +16,7 @@
 
 package com.github.danielfernandez.matchday.web.view;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -50,116 +51,202 @@ import reactor.core.publisher.Mono;
  */
 public class ReactiveMustacheView extends MustacheView {
 
-    private Compiler compiler;
+	private Compiler compiler;
 
-    private String charset;
+	private String charset;
 
-    /**
-     * Set the JMustache compiler to be used by this view. Typically this property is not
-     * set directly. Instead a single {@link Compiler} is expected in the Spring
-     * application context which is used to compile Mustache templates.
-     * @param compiler the Mustache compiler
-     */
-    public void setCompiler(Compiler compiler) {
-        this.compiler = compiler;
-    }
+	/**
+	 * Set the JMustache compiler to be used by this view. Typically this property is not
+	 * set directly. Instead a single {@link Compiler} is expected in the Spring
+	 * application context which is used to compile Mustache templates.
+	 * @param compiler the Mustache compiler
+	 */
+	public void setCompiler(Compiler compiler) {
+		this.compiler = compiler;
+	}
 
-    /**
-     * Set the charset used for reading Mustache template files.
-     * @param charset the charset to use for reading template files
-     */
-    public void setCharset(String charset) {
-        this.charset = charset;
-    }
+	/**
+	 * Set the charset used for reading Mustache template files.
+	 * @param charset the charset to use for reading template files
+	 */
+	public void setCharset(String charset) {
+		this.charset = charset;
+	}
 
-    @Override
-    public boolean checkResourceExists(Locale locale) throws Exception {
-        return resolveResource() != null;
-    }
+	@Override
+	public boolean checkResourceExists(Locale locale) throws Exception {
+		return resolveResource() != null;
+	}
 
-    @Override
-    protected Mono<Void> renderInternal(Map<String, Object> model, MediaType contentType,
-            ServerWebExchange exchange) {
-        Resource resource = resolveResource();
-        if (resource == null) {
-            return Mono.error(new IllegalStateException(
-                    "Could not find Mustache template with URL [" + getUrl() + "]"));
-        }
-        Charset charset = getCharset(contentType).orElse(getDefaultCharset());
-        try (FluxWriter writer = new FluxWriter(
-                () -> exchange.getResponse().bufferFactory().allocateBuffer(), charset)) {
-            try (Reader reader = getReader(resource)) {
-                Template template = this.compiler.compile(reader);
-                template.execute(model, writer);
-                return exchange.getResponse()
-                        .writeAndFlushWith(Flux.from(writer.getBuffers()));
-            }
-            catch (Exception ex) {
-                writer.release();
-                return Mono.error(ex);
-            }
-        }
-        catch (Exception ex) {
-            return Mono.error(ex);
-        }
-    }
+	@Override
+	protected Mono<Void> renderInternal(Map<String, Object> model, MediaType contentType,
+			ServerWebExchange exchange) {
+		Resource resource = resolveResource();
+		if (resource == null) {
+			return Mono.error(new IllegalStateException(
+					"Could not find Mustache template with URL [" + getUrl() + "]"));
+		}
+		boolean sse = MediaType.TEXT_EVENT_STREAM.isCompatibleWith(contentType);
+		Charset charset = getCharset(contentType).orElse(getDefaultCharset());
+		try (FluxWriter writer = new FluxWriter(
+				() -> exchange.getResponse().bufferFactory().allocateBuffer(), charset)) {
+			try (Reader reader = getReader(resource, sse)) {
+				Template template = this.compiler.compile(reader);
+				template.execute(model, writer);
+				return exchange.getResponse()
+						.writeAndFlushWith(Flux.from(writer.getBuffers()));
+			}
+			catch (Exception ex) {
+				writer.release();
+				return Mono.error(ex);
+			}
+		}
+		catch (Exception ex) {
+			return Mono.error(ex);
+		}
+	}
 
-    @Override
-    protected Mono<Void> resolveAsyncAttributes(Map<String, Object> model) {
-        Map<String, Object> result = new HashMap<>();
-        for (String key : model.keySet()) {
-            if (!key.startsWith("flux") && !key.startsWith("mono")
-                    && !key.startsWith("publisher")) {
-                result.put(key, model.get(key));
-            }
-            else {
-                model.put(key, new FluxLambda((Publisher<?>) model.get(key)));
-            }
-        }
-        return super.resolveAsyncAttributes(result)
-                .doOnSuccess(v -> model.putAll(result));
-    }
+	@Override
+	protected Mono<Void> resolveAsyncAttributes(Map<String, Object> model) {
+		Map<String, Object> result = new HashMap<>();
+		for (String key : model.keySet()) {
+			if (!key.startsWith("flux") && !key.startsWith("mono")
+					&& !key.startsWith("publisher")) {
+				result.put(key, model.get(key));
+			}
+			else {
+				model.put(key, new FluxLambda((Publisher<?>) model.get(key)));
+			}
+		}
+		return super.resolveAsyncAttributes(result)
+				.doOnSuccess(v -> model.putAll(result));
+	}
 
-    private Resource resolveResource() {
-        Resource resource = getApplicationContext().getResource(getUrl());
-        if (resource == null || !resource.exists()) {
-            return null;
-        }
-        return resource;
-    }
+	private Resource resolveResource() {
+		Resource resource = getApplicationContext().getResource(getUrl());
+		if (resource == null || !resource.exists()) {
+			return null;
+		}
+		return resource;
+	}
 
-    private Reader getReader(Resource resource) throws IOException {
-        if (this.charset != null) {
-            return new InputStreamReader(resource.getInputStream(), this.charset);
-        }
-        return new InputStreamReader(resource.getInputStream());
-    }
+	private Reader getReader(Resource resource, boolean sse) throws IOException {
+		Reader result;
+		if (this.charset != null) {
+			result = new InputStreamReader(resource.getInputStream(), this.charset);
+		}
+		else {
+			result = new InputStreamReader(resource.getInputStream());
+		}
+		if (sse) {
+			Reader unclosed = sse(result);
+			result = unclosed;
+		}
+		return result;
+	}
 
-    private Optional<Charset> getCharset(MediaType mediaType) {
-        return Optional.ofNullable(mediaType != null ? mediaType.getCharset() : null);
-    }
+	private Reader sse(Reader result) {
+		return new SseTemplateReader(result);
+	}
 
-    private static class FluxLambda implements Mustache.Lambda {
+	private Optional<Charset> getCharset(MediaType mediaType) {
+		return Optional.ofNullable(mediaType != null ? mediaType.getCharset() : null);
+	}
 
-        private Publisher<?> publisher;
+	private static class FluxLambda implements Mustache.Lambda {
 
-        public FluxLambda(Publisher<?> publisher) {
-            this.publisher = publisher;
-        }
+		private Publisher<?> publisher;
 
-        @Override
-        public void execute(Fragment frag, Writer out) throws IOException {
-            if (out instanceof FluxWriter) {
-                FluxWriter fluxWriter = (FluxWriter) out;
-                fluxWriter.flush();
-                fluxWriter.write(Flux.from(publisher).map(value -> wrap(frag.execute(value))));
-            }
-        }
+		public FluxLambda(Publisher<?> publisher) {
+			this.publisher = publisher;
+		}
 
-        private String wrap(String value) {
-            return value;
-        }
+		@Override
+		public void execute(Fragment frag, Writer out) throws IOException {
+			if (out instanceof FluxWriter) {
+				FluxWriter fluxWriter = (FluxWriter) out;
+				fluxWriter.flush();
+				fluxWriter.write(Flux.from(publisher).map(value -> frag.execute(value)));
+			}
+		}
 
-    }
+	}
+
+}
+
+class SseTemplateReader extends BufferedReader {
+
+	public SseTemplateReader(Reader in) {
+		super(new SseReader(in));
+	}
+
+	static class SseReader extends Reader {
+
+		private char[] line;
+		private String flux;
+		int pointer;
+		private BufferedReader origin;
+
+		public SseReader(Reader in) {
+			this.origin = new BufferedReader(in);
+		}
+
+		@Override
+		public void close() throws IOException {
+			origin.close();
+		}
+
+		public int read(char cbuf[], int off, int len) throws IOException {
+			int total = 0;
+			if (line == null) {
+				line = next();
+			}
+			int size = len;
+			while (line != null) {
+				if (size >= line.length - pointer) {
+					System.arraycopy(line, pointer, cbuf, off, line.length - pointer);
+					size -= line.length - pointer;
+					total += line.length - pointer;
+					off += line.length - pointer;
+					line = next();
+					pointer = 0;
+				}
+				else {
+					if (size > 0) {
+						System.arraycopy(line, pointer, cbuf, off, size);
+						total += size;
+						pointer += size;
+					}
+					// we wrote the whole buffer
+					break;
+				}
+			}
+			return total == 0 && line==null ? -1 : total;
+		}
+
+		private char[] next() throws IOException {
+			String line = origin.readLine();
+			if (this.line == null) {
+				while (line != null && line.trim().length() == 0) {
+					// first non-empty line
+					line = origin.readLine();
+				}
+				if (line != null) {
+					// first line
+					this.flux = line;
+					line = line + "\nevent: message\nid: {{id}}\n";
+				}
+			}
+			else if (flux != null && line != null
+					&& line.trim().equals(flux.replace("#", "/"))) {
+				// last line has 2 empty lines before it
+				line = "\n\n" + line + "\n";
+			}
+			else if (line != null) {
+				line = "data: " + line + "\n";
+			}
+			return line == null ? null : line.toCharArray();
+		}
+	}
 
 }
